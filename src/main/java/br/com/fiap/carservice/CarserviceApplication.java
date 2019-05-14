6dd630a604fa8d7379b25f29053c05e00a5032da
Danilo Vitoriano <dvitoriano@uolinc.com>
2019-05-14 18:13:04 -0300

    initial config

---
+++ src/main/java/br/com/fiap/carservice/CarserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.carservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/carservice/CarserviceApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.fiap.carservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CarserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
