0efed650a6b79fcb7583bd6a241e974e115f79a2
Danilo Vitoriano <dvitoriano@uolinc.com>
2019-05-14 18:31:08 -0300

    add mockito and car resource

---
+++ src/main/java/br/com/fiap/carservice/CarResource.java
@@ -0,0 +1,4 @@
+package br.com.fiap.carservice;
+
+public interface CarResource {
+}
---
+++ src/test/java/br/com/fiap/carservice/CarResourceMockTest.java
@@ -0,0 +1,16 @@
+package br.com.fiap.carservice;
+
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+// use mockito to run tests
+@RunWith(MockitoJUnitRunner.class)
+public class CarResourceMockTest {
+
+    @Mock // mock resource
+    private CarResource carResource;
+
+
+
+}
