3a561dfe2194ae80681edf162d8bbd78be71ce20
Danilo Vitoriano <dvitoriano@uolinc.com>
2019-05-14 20:17:52 -0300

    add mockito test find car by color method

--- src/test/java/br/com/fiap/carservice/CarResourceMockTest.java
+++ src/test/java/br/com/fiap/carservice/CarResourceMockTest.java
@@ -3,7 +3,11 @@ package br.com.fiap.carservice;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+import java.util.List;
 
 // use mockito to run tests
 @RunWith(MockitoJUnitRunner.class)
@@ -16,11 +20,22 @@ public class CarResourceMockTest {
     @Test // junit test
     public void shouldFindCarByColor(){
         // Stub
+        // car attributes
         String color = "yellow";
         String model = "camaro";
+        // new Car object
         Car car = new Car();
+        // setter methods
         car.setColor(color);
         car.setModel(model);
+
+        // create empty list of Car
+        List<Car> cars = Arrays.asList(car);
+
+        //teste mockito
+        // quando for ao carResource e procurar pela cor
+        // retornar os carros encontrados na lista cars
+        Mockito.when(carResource.findCarByColor(color).thenReturn(cars));
     }
 
 
