cbf62d483d09f10945295d39f4d93e18279205fe
Danilo Vitoriano <dvitoriano@uolinc.com>
2019-05-14 22:13:44 -0300

    convert car resource into spring rest repository resource

--- src/main/java/br/com/fiap/carservice/CarResource.java
+++ src/main/java/br/com/fiap/carservice/CarResource.java
@@ -1,4 +1,17 @@
 package br.com.fiap.carservice;
 
-public interface CarResource {
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.List;
+
+// repository custom mapping and rel
+// path
+// collection resource
+@RepositoryRestResource(path = "cars",collectionResourceRel = "cars")
+public interface CarResource extends PagingAndSortingRepository<Car, Integer> {
+
+    // a list of cars method find by color param
+    List<Car> findCarByColor(@Param("color") String color);
 }
